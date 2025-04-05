8490a99d5fa80fecf21ef441df4e09fb74eb3619
nik5033 <gudkov.nikita@bk.ru>
2025-04-05 15:09:58 +0300

    Погода

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 import os
 
 import telebot
+from requests import get
+import json
 
 API_TOKEN = os.environ['TELEBOT_TOKEN']
+API = os.environ['WEATHER_TOKEN']
 bot = telebot.TeleBot(API_TOKEN)
 
+def parse_json(json_data):
+    temp = json_data['main']['temp']
+    temp_like = json_data['main']['feels_like']
+    pressuse = json_data['main']['pressure']
+    wind_speed = json_data['wind']['speed']
+
+    result_str = f'Температура: {temp}\n \
+                   Чувствуется как: {temp_like}\n \
+                   Давление: {pressuse}\n \
+                   Скорость ветра: {wind_speed}'
+    
+    return result_str
+
+def get_city(message):
+    city = message.text
+
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&lang=ru'
+    responce = get(url)
+
+    parsed_json = parse_json(json.loads(responce.text))
+
+    bot.send_message(message.from_user.id, parsed_json)
+
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def start_handler(message):
     bot.reply_to(message, f'Hi {message.from_user.first_name}')
+
+@bot.message_handler(commands=['weather'])
+def weather_handler(message):
+    bot.send_message(message.from_user.id, 'Введите город:')
+    bot.register_next_step_handler(message, get_city)
 
 bot.infinity_polling()
